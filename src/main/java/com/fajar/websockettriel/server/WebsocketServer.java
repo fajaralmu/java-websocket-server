ad8cbf6cc9ff139ddf27d237b4a1e6fe97b143ec
Fajar AM <somabangsa@gmail.com>
2020-08-22 15:28:55 +0700

    handle not found session

--- src/main/java/com/fajar/websockettriel/server/WebsocketServer.java
+++ src/main/java/com/fajar/websockettriel/server/WebsocketServer.java
@@ -29,23 +29,22 @@ public class WebsocketServer {
 	public void onOpen(Session session) {
 
 		logger.info("Connected ... " + session.getId());
-		
+
 		SESSION_MAP.put(session.getId(), session);
 		try {
-			session.getBasicRemote().sendText("[ID]"+session.getId());
+			session.getBasicRemote().sendText("[ID]" + session.getId());
 		} catch (IOException e) {
-			 
+
 			e.printStackTrace();
 		}
-		
-		logger.info("Current Connected: "+SESSION_MAP.size());
+
+		logger.info("Current Connected: " + SESSION_MAP.size());
 
 	}
 
 	@OnMessage
-
 	public void onMessage(String message, Session session) {
-
+		System.out.println("SERVER ONMessage [" + session.getId() + "]: " + message);
 		if (message.equals("quit")) {
 
 			try {
@@ -58,18 +57,18 @@ public class WebsocketServer {
 
 			}
 
-		} 
-		
+		}
+
 		try {
 			Message messageObj = MessageMapper.getMessage(message);
 			sendMessageTo(messageObj, session);
 		} catch (Exception e) {
-			System.out.println("NOT a Message DTO: "+ session.getId());
- 
+			System.out.println("NOT a Message DTO: " + session.getId());
+
 			try {
 				session.getBasicRemote().sendText(message);
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
+
 				e1.printStackTrace();
 			}
 		}
@@ -81,19 +80,21 @@ public class WebsocketServer {
 //		}
 
 	}
-	
+
 	private void sendMessageTo(Message message, Session sender) throws IOException {
-		 String destination = message.getMessageTo(); 
-		 String newMsg = MessageMapper.constructMessage(sender, destination, message.getMessage());
-		 
-		 Session destinationSession = SESSION_MAP.get(destination);
-		 if(null == destinationSession) {
-			 System.out.println("destinationSession is NULL");
-			 sender.getBasicRemote().sendText("Destination ("+destination+") NOT FOUND");
-			 return;
-		 }
-		 destinationSession.getBasicRemote().sendText(newMsg);
-		 
+		String destination = message.getMessageTo();
+		String newMsg = MessageMapper.constructMessage(sender, destination, message.getMessage());
+
+		Session destinationSession = SESSION_MAP.get(destination);
+		if (null == destinationSession) {
+			System.out.println("destinationSession is NULL");
+			String msg = MessageMapper.OBJECT_MAPPER.writeValueAsString(
+					new Message(sender.getId(), "SERVER", "Destination (" + destination + ") NOT FOUND", new Date()));
+			sender.getBasicRemote().sendText(msg);
+			return;
+		}
+		destinationSession.getBasicRemote().sendText(newMsg);
+
 	}
 
 	@OnClose
